344d946aa5b702cf481ef6a36eb3e832eb59ad00
Tomasz Dłuski <mr.toumash@gmail.com>
2020-03-22 12:09:36 +0100

    moves front to the public folderś

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const socketIO = require('socket.io');
+
 
 let cachedDb = null
 
@@ -13,7 +17,7 @@ async function connectToDatabase(uri) {
   return db;
 }
 
-module.exports = async (req, res) => {
+let testFn = async (req, res) => {
   // Get a database connection, cached or otherwise,
   // using the connection string environment variable as the argument
   const db = await connectToDatabase(process.env.MONGODB_URI)
@@ -27,3 +31,19 @@ module.exports = async (req, res) => {
   // Respond with a JSON string of all users in the collection
   res.status(200).json({ users })
 }
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use(express.static('public'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
