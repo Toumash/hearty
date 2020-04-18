66c7d1dc0a287cf9bda49ddf46fb69e847c66053
Tomasz Dłuski <mr.toumash@gmail.com>
2020-04-18 23:06:12 +0200

    connects mongodb
    needs testing

--- sql.js
+++ sql.js
@@ -13,27 +13,29 @@ async function connectToDatabase(uri) {
   cachedDb = db;
   return db;
 }
-async function users() {
+async function get_users() {
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const collection = await db.collection("users");
   return collection;
 }
 
 async function getUser(id) {
-  const users = await users();
-  return await users.find({ _id: id }).toArray().shift();
+  const users = await get_users();
+  return await users.findOne({ _id: id });;
 }
 async function getAllUsers() {
-  const users = await users();
+  const users = await get_users();
   return await users.find({}).toArray();
 }
 
 async function addUser(user) {
-  const users = await users();
+  const users = await get_users();
   return await users.insertOne(user);
 }
 
 async function updateUser(user) {
-  const users = await users();
-  return await users.replaceOne({ _id: user.id }, user);
+  const users = await get_users();
+  return await users.replaceOne({ _id: user._id }, user);
 }
+
+module.exports = { connectToDatabase, get_users, getUser, getAllUsers, addUser, updateUser };
